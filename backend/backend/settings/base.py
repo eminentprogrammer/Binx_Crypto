ce203c393d69ca3345942b572135facd41239f79
Igwesi Emmanuel Ugochukwu <emmaigwesi@gmail.com>
2024-01-23 20:44:04 +0100

    Initial

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -68,17 +67,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -111,9 +99,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / 'plugins/static')
+    os.path.join(BASE_DIR, 'plugins/static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR / 'plugins/assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'plugins/assets')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', include('apps.api.urls')),
@@ -7,3 +10,5 @@ urlpatterns = [
     path('account/', include('apps.accounts.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
